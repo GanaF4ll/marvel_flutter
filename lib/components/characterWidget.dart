dfb17089ff76aa645967d6684c0da9e4c4a0091f
GanaF4ll <ganafall9498@gmail.com>
2024-06-21 10:00:48 +0200

    :lipstick:

--- lib/components/characterWidget.dart
+++ lib/components/characterWidget.dart
@@ -15,28 +15,50 @@ class CharacterWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Image.network(imageUrl),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              name,
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.network(
+                imageUrl,
+                width: 80,
+                height: 80,
+                fit: BoxFit.cover,
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Text(
-              description.isNotEmpty ? description : 'No description available',
-              style: TextStyle(fontSize: 16),
+            SizedBox(width: 8),
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Expanded(
+                        child: Text(
+                          name,
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                      // Icon(Icons.star, color: Colors.yellow),
+                    ],
+                  ),
+                  SizedBox(height: 8),
+                  Text(
+                    description.isNotEmpty
+                        ? description
+                        : 'No description available',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
